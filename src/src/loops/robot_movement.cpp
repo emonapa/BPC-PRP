aa1a8a78b7d835bfa3a916777dc74877340e1235
tomasuda <xsudato00@stud.fit.vutbr.cz>
2026-05-07 11:09:24 +0200

    exam pass 37s with treasure

--- src/src/loops/robot_movement.cpp
+++ src/src/loops/robot_movement.cpp
@@ -1,6 +1,6 @@
 #include "loops/robot_movement.hpp"
 #include "algorithms/lidar_alg.hpp"
-#include <opencv2/opencv.hpp> // PŘIDÁNO PRO KAMERU
+#include <opencv2/opencv.hpp> /
 #include <algorithm>
 #include <cmath>
 #define SPEED 155
@@ -9,8 +9,8 @@
 namespace loops {
 
 MovementLoop::MovementLoop() : rclcpp::Node("robot_movement_node"),
-    wall_pid_(20.0f, 0.0f, 25.0f), // PID pro držení se zdi
-    turn_pid_(25.0f, 0.0f, 8.0f)   // PID pro přesné otáčení na úhel
+    wall_pid_(19.0f, 0.0f, 22.0f), 
+    turn_pid_(25.0f, 0.0f, 8.0f)  
 {
     // Odběr LiDARu
     lidar_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
@@ -50,13 +50,12 @@ void MovementLoop::camera_callback(const sensor_msgs::msg::CompressedImage::Shar
         auto markers = aruco_detector_.detect(frame);
         
         for (auto& m : markers) {
-            // Treasure (10, 11, 12) má prioritu pred Exit (0, 1, 2)
             if (m.id >= 10 && m.id <= 12) {
                 stored_decision_ = m.id;
                 RCLCPP_INFO(this->get_logger(), "Ulozeny TREASURE kod: %d", m.id);
-                break; // Treasure má top prioritu, končíme hľadanie
+                break; 
             } else if (m.id >= 0 && m.id <= 2) {
-                if (stored_decision_ < 10) { // Prepíš len ak nemáme uložený Treasure
+                if (stored_decision_ < 10) { 
                     stored_decision_ = m.id;
                     RCLCPP_INFO(this->get_logger(), "Ulozeny EXIT kod: %d", m.id);
                 }
@@ -71,7 +70,6 @@ void MovementLoop::lidar_callback(const sensor_msgs::msg::LaserScan::SharedPtr m
     latest_scan_ = msg;
 }
 
-// Zde se jen na pozadí počítá úhel z gyroskopu
 void MovementLoop::imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg) {
     rclcpp::Time now = msg->header.stamp;
     if (first_imu_) {
@@ -88,7 +86,7 @@ void MovementLoop::imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg) {
 
     if (current_state_ == MazeState::CALIBRATION) {
         gyro_calibration_samples_.push_back(gyro_z);
-        if (gyro_calibration_samples_.size() >= 100) { // Cca 2 sekundy
+        if (gyro_calibration_samples_.size() >= 100) { 
             imu_integrator_.setCalibration(gyro_calibration_samples_);
             current_state_ = MazeState::CORRIDOR_FOLLOWING;
             RCLCPP_INFO(this->get_logger(), "Kalibrace HOTOVA! Jedu do bludiste.");
@@ -123,32 +121,28 @@ float R = results.right;
 float F = results.front;
 
 
-const float THRESH_HIGH = 0.42f; // Musí byť VIAC ako toto, aby sme si boli istí, že je voľno
-const float THRESH_LOW  = 0.35f; // Musí klesnúť POD toto, aby sme povedali, že tam niečo je
+const float THRESH_HIGH = 0.42f; 
+const float THRESH_LOW  = 0.35f; 
 
 
-if (L > (last_left_seen_ >= 2 ? 0.38f : THRESH_HIGH)) {
-    if (last_left_seen_ < 2) last_left_seen_++; 
+if (L > (last_left_seen_ >= 1 ? 0.38f : THRESH_HIGH)) {
+    if (last_left_seen_ < 1) last_left_seen_++; 
 } else {
     if (last_left_seen_ > 0) last_left_seen_--;
 }
 
 
-if (R > (last_right_seen_ >= 2 ? 0.38f : THRESH_HIGH)) {
-    if (last_right_seen_ < 2) last_right_seen_++;
+if (R > (last_right_seen_ >= 1 ? 0.38f : THRESH_HIGH)) {
+    if (last_right_seen_ < 1) last_right_seen_++;
 } else {
     if (last_right_seen_ > 0) last_right_seen_--;
 }
 
-// -------------------------
-// LOGIKA STAVOV (Kľúčová zmena)
-// -------------------------
-// Cesta sa považuje za otvorenú až keď sme si 100% istí (counter dosiahol 5)
-bool is_left_open  = (last_left_seen_ >= 2);
-bool is_right_open = (last_right_seen_ >= 2);
+bool is_left_open  = (last_left_seen_ >= 1);
+bool is_right_open = (last_right_seen_ >= 1);
 
 
-const float FRONT_BLOCK_HIGH = 0.25f; // Prah pre detekciu steny (aktivácia)
+const float FRONT_BLOCK_HIGH = 0.29f; 
 const float FRONT_BLOCK_LOW  = 0.45f;
 if (front_blocked_state_) {
     if (F > FRONT_BLOCK_LOW) front_blocked_state_ = false;
@@ -159,24 +153,17 @@ if (front_blocked_state_) {
 bool is_front_blocked = front_blocked_state_;
 
 bool is_front_close =F< FRONT_BLOCK_LOW;
-// -------------------------
-// IGNORE AFTER TURN
-// -------------------------
+
 if (ignore_side_counters_ > 0) {
     float error_center = std::clamp(static_cast<int>(L), 0, 30) - std::clamp(static_cast<int>(R), 0, 30); ;
     float steering = wall_pid_.step(error_center, 0.05f);
-    // set_speed(145 - static_cast<int>(steering), 145 + static_cast<int>(steering));
     set_speed(SPEED,SPEED);
     ignore_side_counters_ --;
     return;
 }
 
-    // Identifikácia situácie
     int open_paths = (is_left_open ? 1 : 0) + (is_right_open ? 1 : 0) + (F > 0.35f ? 1 : 0);
     float current_yaw = imu_integrator_.getYaw();
-    // ROZHODOVANIE - Spustí sa len ak:
-    // 1. Je pred nami stena (T-križovatka alebo zákruta)
-    // 2. Sú OTOVRENÉ OBE STRANY (X-križovatka) - tu predpokladáme, že sme v strede
     if(is_front_close){
     if (is_front_blocked) {
         
@@ -185,7 +172,6 @@ if (ignore_side_counters_ > 0) {
         bool state_changed = false;
         bool state_turn =false;
 
-        // KRIŽOVATKA (viac ako 1 cesta vpred/do boku)
         if (open_paths >= 2) {
             RCLCPP_INFO(this->get_logger(), "Krizovatka %d %d %d",direction,is_left_open,is_right_open);
             if (direction == 1 && is_left_open) {
@@ -212,7 +198,7 @@ if (ignore_side_counters_ > 0) {
                  RCLCPP_INFO(this->get_logger(), "right %d",direction);
                 state_changed = true;
             } else {
-                target_yaw_ = normalize_angle(current_yaw + M_PI); // Otočka
+                target_yaw_ = normalize_angle(current_yaw + M_PI); 
                  RCLCPP_INFO(this->get_logger(), "otocka %d",direction);
                 state_changed = true;
             }
@@ -220,8 +206,7 @@ if (ignore_side_counters_ > 0) {
 
         if (state_changed) {
             RCLCPP_INFO(this->get_logger(), "CHANGING STATE ");
-            // set_speed(127, 127);
-            turn_start_delay_ = 0; // cca 150ms (3 * 50ms)
+            turn_start_delay_ = 2; 
             current_state_ = MazeState::TURNING;
             if(!state_turn) stored_decision_ = -1;
             return;
@@ -246,7 +231,7 @@ if (ignore_side_counters_ > 0) {
             state_changed = true;
         }
         else{
-        //   target_yaw_ =current_yaw;
+          target_yaw_ =current_yaw;
             state_changed = true;
             steering = 0.0f;
         }
@@ -257,8 +242,7 @@ if (ignore_side_counters_ > 0) {
             state_changed = true;
         }
         else {
-            // target_yaw_ =current_yaw;
-           state_changed = true;
+            state_changed = true;
             steering = 0.0f;
             }
     } else if (is_right_open) {
@@ -268,8 +252,6 @@ if (ignore_side_counters_ > 0) {
                 state_changed = true;
         }
         else{
-           //  RCLCPP_INFO(this->get_logger(), "base %d",direction);
-            // target_yaw_ =current_yaw;
             state_changed = true;
             steering = 0.0f;
         }
@@ -281,7 +263,7 @@ if (ignore_side_counters_ > 0) {
      if (state_changed) {
          RCLCPP_INFO(this->get_logger(), "CHANGING STATE  1");
             // set_speed(127, 127);
-            turn_start_delay_ = 7; // cca 150ms (3 * 50ms)
+            turn_start_delay_ = 6; // cca 150ms (3 * 50ms)
             current_state_ = MazeState::TURNING;
             stored_decision_ = -1;
             return;
@@ -305,7 +287,7 @@ case MazeState::TURNING: {
          RCLCPP_INFO(this->get_logger(), "IN TURNING RESET "); // Tolerancia
         set_speed(127, 127);
         // KONIEC OTÁČANIA
-        ignore_side_counters_ = 27;
+        ignore_side_counters_ = 30;
 
         current_state_ = MazeState::CORRIDOR_FOLLOWING;
         turn_pid_.reset();
@@ -316,7 +298,7 @@ case MazeState::TURNING: {
     int correction = std::clamp(static_cast<int>(turn_speed), -40, 40); // Zvýšený rozsah
 
     // Jemnejší deadband - ak je chyba malá, zmenši aj minimálnu silu
-    int min_force = (std::abs(yaw_error) < 0.2f) ? 10 : 15; 
+    int min_force = (std::abs(yaw_error) < 0.2f) ? 9 : 14; 
     if (std::abs(correction) < min_force) {
         correction = (yaw_error > 0) ? min_force : -min_force;
     }
